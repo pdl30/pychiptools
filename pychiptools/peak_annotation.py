8a2d29bbe470dfa495e6e79d77b5978e6a48870b
Patrick LOmbard <pdl30@cam.ac.uk>
2015-02-03 15:45:09 +0000

    updated peak anno

--- pychiptools/peak_annotation.py
+++ pychiptools/peak_annotation.py
@@ -15,8 +15,12 @@ import subprocess
 from operator import itemgetter
 import HTSeq
 import argparse
+import tempfile
+import rpy2.robjects as ro
+from rpy2.robjects.packages import importr
+from rpy2.robjects.vectors import IntVector, FloatVector, StrVector
 
-#Use GTF for this
+
 def read_annotation(gtf):
 	gene_info = {}
 	chrom = {}
@@ -43,19 +47,14 @@ def f7(seq):
     seen_add = seen.add
     return [ x for x in seq if not (x in seen or seen_add(x))]
 
-def read_peak_info(peak_file, ens):
-	#Since 
+def read_peak_info(peak_file):
 	peak_data = {}
 	count = 0
 	with open(peak_file) as f:
 		for line in f:
 			line = line.rstrip()
 			word = line.split("\t")
-			if ens:
-				chrom = word[0].strip("chr")
-				peak_data[count]=(chrom, word[1], word[2])
-			else:
-				peak_data[count]=(word[0], word[1], word[2])
+			peak_data[count]=(word[0], word[1], word[2])
 			count +=1
 	return peak_data
 
@@ -119,7 +118,6 @@ def homer_annotation(peak, genome, output, gtf=None):
 		command = "annotatePeaks.pl {} {} -gtf {} > {}".format(peak, genome, gtf, output)
 	else:
 		command = "annotatePeaks.pl {} {} > {}".format(peak, genome, output)
-	print command
 	subprocess.call(command, shell=True)
 
 def convert_to_ens(peak_file):
@@ -135,97 +133,121 @@ def convert_to_ens(peak_file):
 			output.write("{}\t{}\t{}\n".format(chrom, word[1], word[2])),
 	return name
 
-def run_rcode(rscript, name):
-	rcode = open(name, "w")
-	rcode.write(rscript)
-	rcode.close()
-	try:
-		subprocess.call(['Rscript', name])
-	except:
-		error("Error in running {}\n".format(name))
-		error("Error: %s\n" % str(sys.exc_info()[1]))
-		error( "[Exception type: %s, raised in %s:%d]\n" % ( sys.exc_info()[1].__class__.__name__, 
-		os.path.basename(traceback.extract_tb( sys.exc_info()[2] )[-1][0]), 
-		traceback.extract_tb( sys.exc_info()[2] )[-1][1] ) )
-		sys.exit(1)
-
-def chippeakanno(peaks, output_prefix):
-	rscript = "library('ChIPpeakAnno')\n"
-	rscript += "library(biomaRt)\n"
-	rscript += "ensembl = useMart('ensembl')\n"
-	rscript += "ensembl = useDataset('mmusculus_gene_ensembl', mart=ensembl)\n"
-	rscript += "TSS.mouse.NCBIM38 = getAnnotation(mart=ensembl, featureType='TSS')\n"
-	rscript += "peaks <- BED2RangedData('{}')\n".format(peaks)
-	rscript += "annotatedPeaktss = annotatePeakInBatch(peaks, AnnotationData=TSS.mouse.NCBIM38, PeakLocForDistance='middle')\n"
-	rscript += "annotatedPeaktrans = annotatePeakInBatch(peaks, AnnotationData=TSS.mouse.NCBIM38, FeatureLocForDistance='middle', PeakLocForDistance='middle', output='shortestDistance')\n"
-	rscript += "annotatedPeaktss <- as.data.frame(annotatedPeaktss)\n"
-	rscript += "annotatedPeaktrans <- as.data.frame(annotatedPeaktrans)\n"
-	rscript += "C1BM <- getBM(c('ensembl_gene_id', 'external_gene_name','description', 'gene_biotype'),filters = 'ensembl_gene_id', values = annotatedPeaktrans$feature, mart = ensembl)\n"
-	rscript += "C2BM <- getBM(c('ensembl_gene_id', 'external_gene_name','description', 'gene_biotype'),filters = 'ensembl_gene_id', values = annotatedPeaktss$feature, mart = ensembl)\n"
-	rscript += "annotrans <- cbind(annotatedPeaktrans, C1BM[match(annotatedPeaktrans$feature, C1BM[,1]), 2:4])\n"
-	rscript += "annotss <- cbind(annotatedPeaktss, C2BM[match(annotatedPeaktss$feature, C2BM[,1]), 2:4])\n"
-	rscript += "write.table(annotss, file='{}_nearest_tss.tsv', sep='\\t', quote=F, row.names=F)\n".format(output_prefix)
-	rscript += "write.table(annotrans, file='{}_nearest_gene.tsv', sep='\\t', quote=F, row.names=F)\n".format(output_prefix)
-	return rscript
-
-def annotate_ensembl(dict_obj):
+def annotate_ensembl(dict_obj, genome, transcript=False):
 	ens = importr("biomaRt")
 	ensembl = ro.r.useMart("ensembl")
-	genome="mmusculus_gene_ensembl"
+	if genome == "mm10":
+		genome="mmusculus_gene_ensembl"
+	elif genome == "hg19":
+		genome="hsapiens_gene_ensembl"
 	ensembl = ro.r.useDataset(genome, mart=ensembl)
 	values = []
-	for key1 in dict_obj.keys():
+	for key1 in dict_obj:
 		values.append(key1)
-	C1BM = ro.r.getBM(attributes=StrVector(["external_gene_id", "description", "gene_biotype"]), filters="ensembl_gene_id", values=values, mart=ensembl)
+	if transcript:
+		C1BM = ro.r.getBM(attributes=StrVector(["ensembl_transcript_id", "external_gene_name", "description", "gene_biotype"]), filters="ensembl_transcript_id", 
+			values=values, mart=ensembl)
+	else:
+		C1BM = ro.r.getBM(attributes=StrVector(["ensembl_gene_id", "external_gene_name", "description", "gene_biotype"]), filters="ensembl_gene_id", values=values, mart=ensembl)
 	gene = list(C1BM.rx(True,1))
-	chr1 = list(C1BM.rx(True,2))
-	tss = list(C1BM.rx(True,3))
-	end = list(C1BM.rx(True,4))
-	st = list(C1BM.rx(True,5))
-	name = list(C1BM.rx(True,6))
-	des = list(C1BM.rx(True,7))
-	bio = list(C1BM.rx(True,8))
+	name = list(C1BM.rx(True,2))
+	des = list(C1BM.rx(True,3))
+	bio = list(C1BM.rx(True,4))
 	data = {}
 	for index, g in enumerate(gene):
-		data[g] = (chr1[index], tss[index], end[index], st[index], name[index], des[index], bio[index])
+		data[g] = (name[index], des[index], bio[index])
 	return data
+
+def get_paths(path1, genome):
+	if genome == "hg19":
+		gtf = path1 + "hg19/hg19.gtf"
+	elif genome == "mm10":
+		gtf = path1 + "mm10/mm10.gtf"
+	return gtf
+
+def closestbed(peak, gtf):
+	f = tempfile.NamedTemporaryFile(delete=False)
+	f.close()
+	command = "closestBed -D ref -a {} -b {} > {}".format(peak, gtf, f.name)
+	subprocess.call(command, shell=True)
+	os.remove(peak)
+	return f.name
+
+def parse_closest(gtf, genome, outname):
+	transcripts = []
+	output = open(outname, "w")
+	print gtf
+	with open(gtf) as f:
+		for line in f:
+			line = line.rstrip()
+			word = line.split("\t")
+			info = word[11].split(";")
+			trans_id = info[0].strip("^gene_id ")
+			trans_id = trans_id.strip("\"")
+			transcripts.append(trans_id)
+	anno = annotate_ensembl(transcripts, genome, transcript=True)
+	with open(gtf) as f:
+		for line in f:
+			line = line.rstrip()
+			word = line.split("\t")
+			info = word[11].split(";")
+			trans_id = info[0].strip("^gene_id ")
+			trans_id = trans_id.strip("\"")
+			if trans_id in anno:
+				output.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(word[0], word[1], word[2], anno[trans_id][0], anno[trans_id][1], anno[trans_id][2], word[12])),
+			else:
+				output.write("{}\t{}\t{}\n".format(word[0], word[1], word[2])),
+
+def make_tmpbed(peak_data):
+	f = tempfile.NamedTemporaryFile(delete=False)
+	for key in peak_data:
+		f.write("{}\t{}\t{}\n".format(peak_data[key][0], peak_data[key][1],peak_data[key][2])),
+	f.close()
+	return f.name #Remember to delete this!
 
 def main():
 	parser = argparse.ArgumentParser(description='Annotation of Peaks.\n')
 	subparsers = parser.add_subparsers(help='Programs included',dest="subparser_name")
 	custom_parser = subparsers.add_parser('custom', help="Runs custom Annotation")
 	homer_parser = subparsers.add_parser('homer', help="Runs annotatePeaks.pl")
-	chip_parser = subparsers.add_parser('chipanno', help="Runs chippeakanno")
-	custom_parser.add_argument('-p', '--peak', help='Peak file in ensembl format', required=False)
-	custom_parser.add_argument('-g', '--gtf', help='GTF file', required=False)
-	custom_parser.add_argument('-e', help='If using ensembl GTF, convert peaks to Ensembl Format', action="store_true", required=False)
-	custom_parser.add_argument('-o', '--output', help='Output name.', required=False)
+	#chip_parser = subparsers.add_parser('chipanno', help="Runs chippeakanno, no longer recommended")
+	#OK new strategy, use closestBed and custom!
+	custom_parser.add_argument('-p', '--peak', help='Peak file in bed format', required=True)
+	custom_parser.add_argument('-g', '--genome', help='genome to use, options are hg19/mm10', required=True)
+	custom_parser.add_argument('-c', action='store_true', help='Find closest gene instead of closest TSS', required=False)
+	custom_parser.add_argument('-o', '--output', help='Output name.', required=True)
 	
 	homer_parser.add_argument('-p', '--peak', help='Peak file', required=True)
 	homer_parser.add_argument('-g', '--genome', help='Genome, options are mm10/hg19', required=True)
 	homer_parser.add_argument('-a', '--gtf', help='Optional GTF file', required=False)
-	homer_parser.add_argument('-e', '--ens', action='store_true', help='If GTF supplied and ensembl, convert peaks to correct format', required=False)
-	homer_parser.add_argument('-o', '--out', help='Output name.', required=False)
+	homer_parser.add_argument('-e', action='store_true', help='If GTF supplied and ensembl, convert peaks to correct format', required=False)
+	homer_parser.add_argument('-o', '--out', help='Output name.', required=True)
 
-	chip_parser.add_argument('-p', '--peak', help='Peak file', required=True)
-	chip_parser.add_argument('-o', '--output', help='Output prefix', required=True)
 	if len(sys.argv)==1:
 		parser.print_help()
 		sys.exit(1)
 	args = vars(parser.parse_args())
+
+	path1 = "/home/patrick/Reference_Genomes/pyngspipe_references/"
+	gtf = get_paths(path1, args["genome"])
+
 	if args["subparser_name"] == "custom":
-		starts, chrom = read_annotation(args["gtf"])
-		peak_data = read_peak_info(args["peak"], args["e"])
-		tss_anno = closest_tss(peak_data, starts, chrom)
-		for key in tss_anno:
-			print "{}\t{}\t{}\t{}\t{}\t{}\n".format(peak_data[key][0], peak_data[key][1], peak_data[key][2], tss_anno[key][0],tss_anno[key][1],tss_anno[key][2] ),
+		if args["c"]:
+			peak_data = read_peak_info(args["peak"])
+			tmpbed = make_tmpbed(peak_data)
+			tmpgtf = closestbed(tmpbed, gtf)
+			parse_closest(tmpgtf, args["genome"], args["output"])
+		else:
+			peak_data = read_peak_info(args["peak"])
+			starts, chrom = read_annotation(args["gtf"])
+			tss_anno = closest_tss(peak_data, starts, chrom)
+			for key in tss_anno:
+				print "{}\t{}\t{}\t{}\t{}\t{}\n".format(peak_data[key][0], peak_data[key][1], peak_data[key][2], tss_anno[key][0],tss_anno[key][1],tss_anno[key][2] ),
 	elif args["subparser_name"] == "homer":
 		if args["ens"]:
 			name = convert_to_ens(args["peak"])
 			homer_annotation(name + "_tmp.bed", args["genome"], args["out"], args["gtf"])
 		else:
 			homer_annotation(args["peak"], args["genome"], args["out"], args["gtf"])
-	elif args["subparser_name"] == "chipanno":
-		rscript = chippeakanno(args["peak"], args["output"])
-		run_rcode(rscript, "chipseqanno_rcode.R")
-		
+
+main()
